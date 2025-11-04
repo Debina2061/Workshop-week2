b0c76b9d9c93fa7d5e873f80a2e604d8ab5006d6
Debina <debinabaraili94@gmail.com>
2025-11-04 16:47:49 +0545

    Task 5

---
+++ Task 5/Program.cs
@@ -0,0 +1,12 @@
+﻿DateTime birthDate = new DateTime(2004, 12, 19); // yyyy, mm, dd
+DateTime currentDate = DateTime.Now;
+
+TimeSpan ageSpan = currentDate - birthDate;
+int ageYears = (int)(ageSpan.Days / 365.25); // approximate age in years
+
+Console.WriteLine($"Birthdate: {birthDate.ToShortDateString()}");
+Console.WriteLine($"Current Date: {currentDate.ToShortDateString()}");
+Console.WriteLine($"Age: {ageYears} years");
+
+DateTime newDate = birthDate.AddDays(10);
+Console.WriteLine($"Birthdate + 10 days: {newDate.ToShortDateString()}");
